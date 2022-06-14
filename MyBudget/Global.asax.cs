00dc0aec0f4e8dfb5c32cea0b8d76fe3162315ae
himanshi-proximous <79898320+himanshi-proximous@users.noreply.github.com>
2022-06-14 17:06:31 +0530

    up

--- MyBudget/Global.asax.cs
+++ MyBudget/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -13,6 +15,7 @@ namespace MyBudget
     {
         protected void Application_Start()
         {
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Email;
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
